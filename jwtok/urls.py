056167862da00109ea478b91161ad3ba7edb1e27
Sachin <sachnaror@gmail.com>
2024-04-16 10:55:52 +0530

    Daily learning: the fuel for lifelong success

---
+++ jwtok/urls.py
@@ -0,0 +1,8 @@
+# Your urls configuration here
+from django.urls import path
+
+from .views import Home
+
+urlpatterns = [
+    path('', Home.as_view()),
+]
--- jwtok/views.py
+++ jwtok/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class Home(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
