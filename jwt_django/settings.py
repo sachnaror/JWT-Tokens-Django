f5e42f9eee2d559874bcf273c46bc7317971e01c
Sachin <sachnaror@gmail.com>
2024-04-16 11:17:37 +0530

    Hard work crafts the path to greatness

--- jwt_django/settings.py
+++ jwt_django/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# https://medium.com/django-unleashed/securing-django-rest-apis-with-jwt-authentication-using-simple-jwt-a-step-by-step-guide-28efa84666fe
